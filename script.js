5d4fb9303c98fac284f3b7e895ea9d1f7b49776e
dm-hussain <message2hussain@gmail.com>
2025-02-06 14:55:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView:3,
+    spaceBetween:20,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".next",
+      prevEl: ".prev",
+    },
+    breakpoints:{
+      '280':{
+        slidesPerView:1
+      },
+      '768':{
+        slidesPerView:2
+      },
+      '1200':{
+        slidesPerView:3
+      }
+    }
+  });
+
+
+
+  // Hide Scroll button***************
+
+
+
+  const scrollBtn= document.querySelector('.scrollBtn')
+
+ document.addEventListener('DOMContentLoaded', ()=>{
+
+  const heroSection= document.querySelector('#hero')
+
+
+  const observer= new IntersectionObserver(([entry])=>{
+
+      if(entry.isIntersecting){
+        scrollBtn.classList.add('hidden')
+      }else{
+        scrollBtn.classList.remove('hidden')
+      }
+      { threshold: 0.5 } // Adjust to detect when 50% of hero is visible
+
+
+
+  })
+  observer.observe(heroSection);
+
+ })
+
+
+//  **************
